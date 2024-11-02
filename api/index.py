53536f3128c9e027547b6d35e19db630bbff955b
Khizar Imran <khzrimrn@gmail.com>
2024-11-02 19:20:59 +0000

    added route to index file

--- api/index.py
+++ api/index.py
@@ -3,5 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def portfolio():
     return render_template('index.html')
+
+@app.route('/article/neural-nets-from-scratch')
+def neural_nets_article():
+    return render_template('neural-nets-article.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
